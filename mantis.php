227c919f7173f70b5e0f7c8e75ba11d536c5f012
hschulz <hschulz@titan.chaos.lan>
2014-08-12 23:03:06 +0200

    Changed the license to BSD-3-Clause. Added license file. Updated the variable naming.

--- mantis.php
+++ mantis.php
@@ -7,16 +7,11 @@
  * Language: PHP 5.4
  * IDE:      Netbeans 7.2
  *
+ * License: http://www.opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ *
+ *
  * Description:
  * ------------
- * (de)
- * Post-Commit-Script zum automatisierten kommentieren und schlie�en von
- * Tickets im Mantis-Bugtracker.
- *
- * Es verlässt sich dabei darauf das Systembefehle durch PHP aufgerufen werden
- * dürfen.
- *
- * (en)
  * Post-Commit script to automate the commenting and closing of Tickets
  * in the Mantis-Bugtracker.
  *
@@ -25,27 +20,21 @@
  *
  * Default PHP behavior
  *
- * php mantis.php /path/to/repo 42
+ * >> php mantis.php /path/to/repo 42
  *
  * or if the file is made executable and renamed to mantis
  *
- * mantis /path/to/repo 42
+ * >> mantis /path/to/repo 42
  *
  * Variables:
  * ----------
- * (de)
- * Werden automatisch von SVN per Hook an das Script übergeben.
- *
- * (en)
  * Are given to the script by the SVN hook.
  *
  * $argv[1] = /path/to/repo // Pfad zum Repository
  * $argv[2] = 123           // Revisionsnummer
  */
 
-/* (de) Einfache �bersetzung
- * (en) Easy translation
- */
+/* Easy translation */
 define('STR_AUTHOR',   'Autor');
 define('STR_REVISION', 'Revision');
 define('STR_LOG',      'Log');
@@ -53,16 +42,13 @@ define('STR_ADDED',    'Hinzugefügt');
 define('STR_MODIFIED', 'Modifiziert');
 define('STR_DELETED',  'Gelöscht');
 
-/* (de) Definition des Mantis-Scripts zum Verarbeiten des Commits
- * (en) Definition of the Mantis script to process commit messages
- */
+/* Definition of the Mantis script to process commit messages */
 define(
     'MANTIS_CHECKIN_SCRIPT',
-    'php -q /home/remind/web/mantis/scripts/checkin.php'
+    'php -q /var/www/web/mantis/scripts/checkin.php'
 );
 
-/* (de) Optionale TRAC-URL die im Kommentar angezeigt wird.
- * (en) Optional TRAC URL displayed in the comment.
+/* Optional TRAC URL displayed in the comment.
  * @link http://trac.edgewall.org/
  */
 //define('STR_TRAC_CHANGESET', 'Änderungen in TRAC ansehen');
@@ -71,192 +57,130 @@ define(
 //    'http://trac.company.com/project/changeset/'
 //);
 
-/* (de) Initialisieren des Arrays das alle geänderten Dateien enthält
- * (en) Initialize the array that contains all modified files
- */
-$aChanged = array();
+/* Initialize the array that contains all modified files */
+$changed = array();
 
-/* (de) Initialisieren des Arrays zur Speicherung der Textbausteine
- * (en) Initialize the array that contains all text blocks
- */
-$aChangeList = array();
+/* Initialize the array that contains all text blocks */
+$changeList = array();
 
-/* (de) Pfad zum Repository auslesen
- * (en) Save the repositorys path
- */
-$sRepositoryPath = $argv[1];
+/* Save the repositorys path */
+$repositoryPath = $argv[1];
 
-/* (de) Revisionsnummer auslesen
- * (en) Save the revision number
- */
-$iRevision = (int)$argv[2];
+/* Save the revision number */
+$revision = (int)$argv[2];
 
-/* (de) Subversion Befehl svnlook date für die Zeitangabe
- * (en) Subversion commant svnlook date to get the timestamp
- */
-$sDate = trim(`svnlook date $sRepositoryPath -r $iRevision`);
+/* Subversion commant svnlook date to get the timestamp */
+$date = trim(`svnlook date $repositoryPath -r $revision`);
 
-/* (de) Subversion Befehl svnlook author für den Author
- * (en) Subversion command svnlook author to get the author
- */
-$sAuthor = trim(`svnlook author $sRepositoryPath -r $iRevision`);
+/* Subversion command svnlook author to get the author */
+$author = trim(`svnlook author $repositoryPath -r $revision`);
 
-/* (de) Subversion Befehl svnlook log für den Kommentar
- * (en) Subversion command svnlook log to get the comment
- */
-$sComment = trim(`svnlook log $sRepositoryPath -r $iRevision`);
+/* Subversion command svnlook log to get the comment */
+$comment = trim(`svnlook log $repositoryPath -r $revision`);
 
-/* (de) Auslesen aller geänderten Dateien
- * (en) Get all changed files
- */
-exec("svnlook changed $sRepositoryPath -r $iRevision", $aChanged);
+/* Get all changed files */
+exec("svnlook changed $repositoryPath -r $revision", $changed);
 
-/* (de) Die geänderten Dateien durchlaufen
- * (en) Iterate the changed files
- */
-foreach ($aChanged as $sChange) {
+/* Iterate the changed files */
+foreach ($changed as $sChange) {
 
-    /* (de) Zeichen für die Veränderung an Dateien A,U etc.
-     * (en) Save the SVN character for file changes
-     */
-    $sChangeSymbol   = $sChange[0];
+    /* Save the SVN character for file changes */
+    $changeSymbol   = $sChange[0];
 
-    /* (de) Zeichen für die Ver�nderung an SVN-Properties
-     * (en) Save the SVN charactger for property changes
-     */
-    $sPropertySymbol = $sChange[1];
+    /* Save the SVN charactger for property changes */
+    $propertySymbol = $sChange[1];
 
-    /* (de) Rest der Zeile enthält die geänderte Datei
-     * (en) The rest of the line contains the file path
-     */
-    $sLine = substr($sChange, 3);
+    /* The rest of the line contains the file path */
+    $line = substr($sChange, 3);
 
-    /* (de) Wenn an den SVN-Properties etwas geändert wurde
-     * (en) If there was an SVN property change
-     */
-    if ($sPropertySymbol == 'U') {
-        $sLine .= ' (props changed)';
+    /* If there was an SVN property change */
+    if ($propertySymbol == 'U') {
+        $line .= ' (props changed)';
     }
 
-    /* (de) Je nach Symbol werden die Änderungen anders zur Ausgabe gespeichert
-     * (en) Depending on the character the changes are saved in a diffrent list
-     */
-    switch ($sChangeSymbol) {
+    /* Depending on the character the changes are saved in a diffrent list */
+    switch ($changeSymbol) {
         case 'A': // File added
-            $aChangeList['added'][] = $sLine;
+            $changeList['added'][] = $line;
             break;
 
         case '_': // Property modified
         case 'U': // File modified
-            $aChangeList['modified'][] = $sLine;
+            $changeList['modified'][] = $line;
             break;
 
         case 'D': // File deleted
-            $aChangeList['deleted'][] = $sLine;
+            $changeList['deleted'][] = $line;
             break;
 
         default:
     }
 }
 
-/* (de) Initialisierung des Kommentars für Mantis
- * (en) Initialize the comment for mantis
- */
-$sMessage = STR_REVISION . ': ' . $iRevision . ' (' . $sDate . ')' . PHP_EOL;
-$sMessage .= STR_AUTHOR . ': ' . $sAuthor . PHP_EOL;
-$sMessage .= PHP_EOL;
+/* Initialize the comment for mantis */
+$message  = STR_REVISION . ': ' . $revision . ' (' . $date . ')' . PHP_EOL;
+$message .= STR_AUTHOR . ': ' . $author . PHP_EOL;
+$message .= PHP_EOL;
 
-/* (de) Wenn die TRAC-Strings definiert sind
- * (en) If the TRAC strings are defined
- */
+/* If the TRAC strings are defined */
 if (defined('STR_TRAC_CHANGESET') && defined('TRAC_CHANGESET_URL')) {
 
-    /* (de) Einen Link zum Commit im TRAC einfügen
-     * (en) Add a link to the commit made in TRAC project
-     */
-    $sMessage .= STR_TRAC_CHANGESET . ': ' . TRAC_CHANGESET_URL . $iRevision;
-    $sMessage .= PHP_EOL . PHP_EOL;
+    /* Add a link to the commit made in TRAC project */
+    $message .= STR_TRAC_CHANGESET . ': ' . TRAC_CHANGESET_URL . $revision;
+    $message .= PHP_EOL . PHP_EOL;
 }
 
-/* (de) Kommentar des Commits ausgeben
- * (en) Display the comment made for the commit
- */
-$sMessage .= STR_LOG . ':' . PHP_EOL;
-$sMessage .= $sComment . PHP_EOL;
+/* Display the comment made for the commit */
+$message .= STR_LOG . ':' . PHP_EOL;
+$message .= $comment . PHP_EOL;
 
-/* (de) Wenn Dateien hinzugefügt wurden
- * (en) If files were added
- */
-if (!empty($aChangeList['added'])) {
+/* If files were added */
+if (!empty($changeList['added'])) {
 
-    /* (de) Den Added-Block hinzuf�gen
-     * (en) Add the added block
-     */
-    $sMessage .= PHP_EOL;
-    $sMessage .= STR_ADDED . ':';
-    $sMessage .= PHP_EOL;
+    /* Add the added block */
+    $message .= PHP_EOL;
+    $message .= STR_ADDED . ':';
+    $message .= PHP_EOL;
 
-    /* (de) Alle hinzugefügten Elemente auflisten
-     * (en) List all added elements
-     */
-    foreach ($aChangeList['added'] as $sLine) {
-        $sMessage .= '    ' . $sLine . PHP_EOL;
+    /* List all added elements */
+    foreach ($changeList['added'] as $line) {
+        $message .= '    ' . $line . PHP_EOL;
     }
 
-    /* (de) Mit einer Newline den Block abschließen
-     * (en) End with a new line
-     */
-    $sMessage .= PHP_EOL;
+    /* End with a new line */
+    $message .= PHP_EOL;
 }
 
-/* (de) Wenn Dateien modifiziert wurden
- * (en) If files were modified
- */
-if (isset($aChangeList['modified'])) {
+/* If files were modified */
+if (isset($changeList['modified'])) {
 
-    /* (de) Den Modifiied-Block hinzufügen
-     * (en) Add the modified block
-     */
-    $sMessage .= PHP_EOL;
-    $sMessage .= STR_MODIFIED . ':';
-    $sMessage .= PHP_EOL;
+    /* Add the modified block */
+    $message .= PHP_EOL;
+    $message .= STR_MODIFIED . ':';
+    $message .= PHP_EOL;
 
-    /* (de) Alle modifizierten Elemente auflisten
-     * (en) List all modified elements
-     */
-    foreach ($aChangeList['modified'] as $sLine) {
-        $sMessage .= '    ' . $sLine . PHP_EOL;
+    /* List all modified elements */
+    foreach ($changeList['modified'] as $line) {
+        $message .= '    ' . $line . PHP_EOL;
     }
 
-    /* (de) Mit einer Newline den Block abschließen
-     * (en) End with a new line
-     */
-    $sMessage .= PHP_EOL;
+    /* End with a new line */
+    $message .= PHP_EOL;
 }
 
-/* (de) Wenn Dateien gelöscht wurden
- * (en) If files were deleted
- */
-if (isset($aChangeList['deleted'])) {
+/* If files were deleted */
+if (isset($changeList['deleted'])) {
 
-    /* (de) Den Deleted-Block hinzuf�gen
-     * (en) Add the deleted block
-     */
-    $sMessage .= PHP_EOL;
-    $sMessage .= STR_DELETED . ':';
-    $sMessage .= PHP_EOL;
+    /* Add the deleted block */
+    $message .= PHP_EOL;
+    $message .= STR_DELETED . ':';
+    $message .= PHP_EOL;
 
-    /* (de) Alle hinzugefügten Elemente auflisten
-     * (en) List all deleted elements
-     */
-    foreach ($aChangeList['added'] as $sLine) {
-        $sMessage .= '    ' . $sLine . PHP_EOL;
+    /* List all deleted elements */
+    foreach ($changeList['added'] as $line) {
+        $message .= '    ' . $line . PHP_EOL;
     }
 }
 
-/* (de) Ausführen des Mantis-Scripts mit dem generierten Nachricht
- * (en) Execute the Mantis script with the generated message
- */
-exec(MANTIS_CHECKIN_SCRIPT . ' <<< "' . $sMessage . '"');
-
-?>
+/* Execute the Mantis script with the generated message */
+exec(MANTIS_CHECKIN_SCRIPT . ' <<< "' . $message . '"');
